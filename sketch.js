b0b0def1693de297953473d50e2994a2736f20f8
mrinal5151 <35590070+mrinal5151@users.noreply.github.com>
2018-01-23 13:03:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,10 +12,11 @@ function setup(){
   createCanvas(400, 400);
 }
 function draw(){
-  background(colour.r, colour.g, colour.b);
+  col = map(mouseX, 0, 400, 0, 255);
+  background(col);
   noStroke();
   fill(255, 0, 0, 100);
   ellipse(width/2, circle.y, circle.diameter, circle.diameter);
   circle.y+=2;
-  colour.r--;colour.g--;colour.b--;
+  //colour.r--;colour.g--;colour.b--;
 }
